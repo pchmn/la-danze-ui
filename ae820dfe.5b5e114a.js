b9d073d24f1518bdd12148b76212e6a4456c6fcf
pchmn <pchmn.dev@gmail.com>
2021-02-13 02:23:44 +0100

    Updates

--- ae820dfe.5b5e114a.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[20],{92:function(a){a.exports=JSON.parse('{"allTagsPath":"/la-danze-ui/blog/tags","slug":"facebook","name":"facebook","count":1,"permalink":"/la-danze-ui/blog/tags/facebook"}')}}]);
---
+++ dab60299.c3716837.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[38],{109:function(a){a.exports=JSON.parse('{"allTagsPath":"/la-danze-ui/blog/tags","slug":"docusaurus","name":"docusaurus","count":3,"permalink":"/la-danze-ui/blog/tags/docusaurus"}')}}]);
