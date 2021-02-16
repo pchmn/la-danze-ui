f2753e576776ef2181252d1a8c78c40f8be08f28
pchmn <pchmn.dev@gmail.com>
2021-02-16 22:10:45 +0100

    Try fix jest on github actions

--- packages/lib/src/setupTests.ts
+++ packages/lib/src/setupTests.ts
@@ -1,6 +1,3 @@
 import '@testing-library/jest-dom/extend-expect';
-import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
-import { configure } from 'enzyme';
 import './testing/testing.utils';
 
-configure({ adapter: new Adapter() });
