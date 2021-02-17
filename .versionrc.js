ce3cf5fefa1d41237d12207b52efd40e00302020
pchmn <pchmn.dev@gmail.com>
2021-02-17 19:13:36 +0100

    fix: test standard-version

---
+++ .versionrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  bumpFiles: [
+    {
+      filename: "package.json",
+      type: "json"
+    },
+    {
+      filename: "packages/lib/package.json",
+      type: "json"
+    },
+    {
+      filename: "packages/demo/package.json",
+      type: "json"
+    },
+    {
+      filename: "packages/doc/package.json",
+      type: "json"
+    }
+  ]
+}
