693316c525f0a3d4dab38012a0503ede4bcc1f97
pchmn <pchmn.dev@gmail.com>
2021-02-27 11:32:11 +0000

    deploy: d938851a470069d8c27f568b3cfba5c9d40b68e8

---
+++ d86b8145.299a92a5.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[42],{112:function(a){a.exports=JSON.parse('{"allTagsPath":"/la-danze-ui/blog/tags","slug":"facebook","name":"facebook","count":1,"permalink":"/la-danze-ui/blog/tags/facebook"}')}}]);
